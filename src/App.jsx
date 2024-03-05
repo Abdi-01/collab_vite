24e39287b08690e3452e8804b9a12521a34b344f
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2024-03-05 10:07:14 +0700

    feat: create component button (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <div>
         <h1>Property Project Landing Page</h1>
+        <Button id="test-btn" title="click" />
       </div>
     </>
   );
